0b96020d31256ff435d557bf032b0ba49ca8f4e6
Anton Shesterov <artimius1990@gmail.com>
2024-11-13 17:57:45 +0400

    v1.1

--- test_app.py
+++ test_app.py
@@ -1,12 +1,13 @@
-# test_app.py
 import pytest
 from app import app
+
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
+
 
 def test_home_page(client):
     response = client.get('/')
